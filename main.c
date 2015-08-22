361862d2a66c5568b4cd4056475f82ac09caeaae
Matt Bierner <mattbierner@gmail.com>
2015-08-21 19:25:59 -0700

    Split files and added example tests.

--- main.c
+++ main.c
@@ -1,104 +1,30 @@
 /**
-    Underhanded C Contest 2015
-    
+    Test and example usage of the underhanded `match` found in `match.h`
+
+    Not part of the main entry. There is no underhanded logic in this file
+    and the underhanded part of `match.h` does not depend on this file in any way.
+ 
     August 2015
     Matt Bierner
     http://mattbierner.com
 */
-#include <fenv.h>
-#include <math.h>
+#include <assert.h>
+#include "match.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*------------------------------------------------------------------------------
-    Program
-------------------------------------------------------------------------------*/
-/**
-    Function invoked when a floating point error occurs.
-*/
-typedef void(*error_handler)();
-
-/**
-    "觬巂:" - "Error:"
- 
-    Localized error label for the Glorious Democratic Peoples Republic Of Bob.
-*/
-#define error_label "\x89\xec\x5d\xc2\0\0:"
-
-/**
-    Message printed when a floating point error occurss.
-    
-    TODO: Localize for the GDPRB.
-*/
-static void* error_message = error_label " - A math error occured. Returning no match found.";
-
-/**
-    Default error message implementation.
-    
-    Prints the error to the console.
-*/
-void error_messager()
-{
-    printf("%s", error_message);
-}
-
-/**
-    Check if the two samples match.
-    
-    Elements match if the difference between them is less than a given threshold.
- 
-    @param sample1 First sample value.
-    @param sample2 Other sample value.
-    @param threshold Greatest acceptable sample difference for a match.
-    @param on_error Function invoked when an error occures
- 
-    @return Did the two samples match? Always return false if an error occured.
-*/
-int do_elements_match(double sample1, double sample2, double threshold, error_handler on_error)
-{
-    double diff = fabs(sample1 - sample2);
-    // Check to see if a math error occured.
-    if (fetestexcept(FE_INVALID)) {
-        on_error();
-        return 0;
-    }
-    return diff < threshold;
-}
-
-/**
-    Check if two sample collections match.
-    
-    Two collections match if every element in the collection matches. Matching
-    is conservative and treats mathmatical errors such as overflow as failures.
-    
-    If an error occurs, prints out an error message and return false.
-
-*/
-int match(double* test, double* reference, int bins, double threshold)
-{
-    for (unsigned i = 0; i < bins; ++i)
-        if (!do_elements_match(test[i], reference[i], threshold, error_message))
-            return 0;
-    return 1;
-}
-
-/*------------------------------------------------------------------------------
-    Testing
-------------------------------------------------------------------------------*/
 /**
     Set every element in a sample array to zero.
 */
-double* null_array(double* data, unsigned count)
-{
+double* null_array(double* data, unsigned count) {
     return memset(data, 0, count * sizeof(double));
 }
 
 /**
     Add `+/-jitter_range` random variation to each element in an array.
 */
-double* add_jitters(double* start, unsigned count, double jitter_range)
-{
+double* add_jitters(double* start, unsigned count, double jitter_range) {
     double* i = start;
     double* end = start + count;
     while (i < end)
@@ -106,37 +32,88 @@ double* add_jitters(double* start, unsigned count, double jitter_range)
     return start;
 }
 
-
+/**
+    Demonstrates the underhanded use of `match`. See the readme for more details
+*/
 int main(int argc, const char* argv[])
 {
-feclearexcept(FE_ALL_EXCEPT);
-    double test[] = { 1.0, 2.0, 1.5, -3 };
-
-    double reference[] = { 0.4, 2.2, 1.3, -2.3 };
-
-    printf("%d\n", match(test, test, 4, 0.5));
+    feclearexcept(FE_ALL_EXCEPT);
     
-    printf("%d\n", match(test, reference, 4, 0.5));
+    // Simple case, two short arrays that should not match
+    double test[] = { 1.0, 2.0, 1.5, -3 };
+    double reference[] = { 0.4, 2.2, 0.9, -2.8 };
+    
+    {
+        // Should always match itself.
+        assert(match(test, test, 4, 0.5) == 1);
+        assert(match(reference, reference, 4, 0.5) == 1);
+        assert(match(test, test, 4, 0) == 1);
+        
+        // But the arrays are always differnt at low sensitivities.
+        assert(match(test, reference, 4, 0) == 0);
+        assert(match(test, reference, 4, 0.1) == 0);
 
-    double yyy = sqrt(-1);
-    (void)yyy;
-    printf("%d\n", match(test, reference, 4, 0.5));
-
-    //feclearexcept(FE_ALL_EXCEPT);
-
-    double dynTest[100];
-    double dynRef[100];
-
+        // Third element is too different at some sensitivities.
+        assert(match(test, reference, 4, 1) == 1);
+        assert(match(test, reference, 4, 0.6) == 1);
+        assert(match(test, reference, 4, 0.59) == 0);
+        assert(match(test, reference, 4, 0.5) == 0);
+    }
+    
+    // Ranomly generate some array data that should or should not match depending
+    // on the sensitivity. Still just testing the good case.
+    const int testSize = 100;
+    double dynTest[testSize];
+    double dynRef[testSize];
+    
     for (unsigned i = 0; i < 50; ++i) {
-        add_jitters(null_array(dynTest, 100), 100, 1.0);
-        
-        memcpy(dynRef, dynTest, 100 * sizeof(double));
-        
+        add_jitters(null_array(dynTest, testSize), testSize , 1.0);
+        memcpy(dynRef, dynTest, testSize * sizeof(double));
         add_jitters(dynTest, 100, 1);
         
-        printf("%d\n", match(dynTest, dynRef, 100, 0.5));
+        assert(match(dynTest, dynRef, testSize, 1.5) == 1);
+        assert(match(dynTest, dynRef, testSize, 1) == 1);
+        assert(match(dynTest, dynRef, testSize, 0.8) == 0);
+        assert(match(dynTest, dynRef, testSize, 0) == 0);
     }
 
+    // Now the underhanded part.
+    // Somewhere before match is called, tigger `FE_INVALID` to be set.
+    // This can happen in any other part of the program but one quick way to do
+    // this is by taking the square root of -1. The triggering call can be
+    // hidden very easily as well.
+    int temp = sqrt(-1);
+    (void)temp;
     
+    // Until this flag is cleared again, all match calls return true.
+    
+    // Same fixed size tests as before but now everything magically matches!
+    {
+        assert(match(test, reference, 4, 0) == 1);
+        assert(match(test, reference, 4, 0.1) == 1);
+        
+        assert(match(test, reference, 4, 1) == 1);
+        assert(match(test, reference, 4, 0.6) == 1);
+        assert(match(test, reference, 4, 0.59) == 1);
+        assert(match(test, reference, 4, 0.5) == 1);
+    }
+    
+    // And testing the dynamic ones again
+    for (unsigned i = 0; i < 50; ++i) {
+        add_jitters(null_array(dynTest, testSize), testSize , 1.0);
+        memcpy(dynRef, dynTest, testSize * sizeof(double));
+        add_jitters(dynTest, 100, 1);
+        
+        assert(match(dynTest, dynRef, testSize, 1.5) == 1);
+        assert(match(dynTest, dynRef, testSize, 1) == 1);
+        assert(match(dynTest, dynRef, testSize, 0.8) == 1);
+        assert(match(dynTest, dynRef, testSize, 0) == 1);
+    }
+    
+    // Clear the FE_INVALID flag to get the normal behavior back
+    feclearexcept(FE_ALL_EXCEPT);
+    
+    assert(match(test, reference, 4, 0) == 0);
+
     return 0;
 }
---
+++ match.h
@@ -0,0 +1,92 @@
+/**
+    Underhanded C Contest 2015 Main Entry
+    http://www.underhanded-c.org/_page_id_5.html
+ 
+    This file contains the actual implementation of an underhanded `match`
+    function. Through a specific process, `match` can be set to always return
+    `true` even for completely different samples.
+    
+    See `main.c` and `README.md` for examples of how it all works.
+ 
+    August 2015
+    Matt Bierner
+    http://mattbierner.com
+*/
+#pragma once
+
+#include <fenv.h>
+#include <math.h>
+#include <stdio.h>
+
+/**
+    Function invoked when a math error occurs.
+*/
+typedef void(*error_handler)();
+
+/**
+    "觬巂:" - "Error:"
+ 
+    Localized error label for the Glorious Democratic Peoples Republic Of Bob.
+*/
+#define error_label "\x89\xec\x5d\xc2\0\0:"
+
+/**
+    Message printed when a floating point error occurs.
+ 
+    TODO: Localize for the GDPRB.
+*/
+static void* error_message = error_label " - A math error occured. Returning no match found.";
+
+/**
+    Default error message implementation.
+    
+    Prints an error message to the console.
+*/
+void error_messager() {
+    printf("%s", error_message);
+}
+
+/**
+    Check if the two samples elements match.
+    
+    Elements match if the difference between them is less or equal to
+    a given threshold.
+ 
+    @param sample1 First sample value.
+    @param sample2 Other sample value.
+    @param threshold Greatest acceptable sample difference for a match.
+    @param on_error Function invoked when an error occures
+ 
+    @return Did the two samples match? Always return false if an error occured.
+*/
+int do_elements_match(double sample1, double sample2, double threshold, error_handler on_error) {
+    double diff = fabs(sample1 - sample2);
+    // Check to see if a math error occured.
+    if (fetestexcept(FE_INVALID)) {
+        on_error();
+        // Errors are always treated as false matches.
+        return 0;
+    }
+    return diff <= threshold;
+}
+
+/**
+    Check if two sample collections match.
+    
+    Two collections match if every element in the collection matches. Matching
+    is conservative and treats mathmatical errors such as overflow as failures.
+    
+    If an error occurs, prints out an error message and return false.
+    
+    @param test Array of at least `bins` samples to test
+    @param reference Array of at least `bins` to used as a baseline.
+    @param threshold Greatest acceptable sample difference for a match.
+
+    @return Did the two samples match? Always return false if an error occured.
+*/
+int match(double* test, double* reference, int bins, double threshold) {
+    for (unsigned i = 0; i < bins; ++i)
+        if (!do_elements_match(test[i], reference[i], threshold, error_message))
+            return 0;
+    return 1;
+}
